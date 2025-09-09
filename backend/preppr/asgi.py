e55e6cf94a18bd91cf9ea264e9b92c6333b45569
OkinoLeiba <okino.leiba@yahoo.com>
2025-09-10 00:13:08 +0100

    add async feature

--- backend/preppr/asgi.py
+++ backend/preppr/asgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'preppr.settings')
 
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'preppr.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django.setup()
+django_asgi_app = get_asgi_application()
+from chat_app.routing import websocket_urlpatterns
+
+
+application = ProtocolTypeRouter ({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
